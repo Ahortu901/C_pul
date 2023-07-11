143a548925630f481ed66dc457947618660a6685
Ahortu901 <aricktech.edu@gmail.com>
2023-07-11 02:17:02 +0000

    const in c++

---
+++ const.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(){
+    const double PI = 3.141509;
+    const int LIGHT_SPEED = 299792458;
+    const int WIDTH = 1092;
+
+    std::cout << PI << std::endl;
+    std::cout << LIGHT_SPEED << std::endl;
+    std::cout << WIDTH << std::endl;
+    return 0;
+}
